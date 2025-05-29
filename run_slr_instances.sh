d597d606c359c4b16b5b6d870e0e1342e6daeec9
Lucas Parada <lucasparada20@gmail.com>
2025-05-29 15:06:56 -0400

    Update run_slr_instances.sh

--- run_slr_instances.sh
+++ run_slr_instances.sh
@@ -1,5 +1,7 @@
 
 # Model can be 1,2,3,5,6,7
+
+# SLR instances sample commands :
 build/exec_tlb mcfp_solver=3 instance_format=trips \
     instance_file=some/path/to/montreal_n801_e15_0.txt \
     scenarios_file_name=some/path/to/montreal_e400.txt \
@@ -19,4 +21,13 @@ build/exec_tlb mcfp_solver=3 instance_format=trips \
     scenarios_file_name=some/path/to/newyork_e400.txt \
     model=3 \
     re_file=re.txt \
+    output_file=targets.txt
+
+# Datner and Kolka (2017) sample commands:
+
+build/tlb_test mcfp_solver=3 instance_format=trips unbiased_estimator=1 \
+    instance_file=some/path/to/boston2_n132_e100.txt \
+    scenarios_file_name=some/path/to/boston2_e400.txt \
+    model=1 \
+    re_file=re.txt 
     output_file=targets.txt
